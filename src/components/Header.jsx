d2fcf3edb0df20915d82766067943b10d89f2362
Saad <66124515+sadsultan@users.noreply.github.com>
2023-09-20 10:04:59 +0500

    Set up scaffolding files

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/troll-face.png';
+import './Header.css';
+
+export default function Header(){
+    return(
+        <>
+            <img src={logo} alt="Troll Face" />
+        </>
+    )
+}
---
+++ src/components/Meme.jsx
@@ -0,0 +1,9 @@
+import './Meme.css';
+
+export default function Meme(){
+    return(
+        <>
+            <h1>MEME</h1>
+        </>
+    )
+}
