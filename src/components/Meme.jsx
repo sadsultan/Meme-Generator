8e44393da13193db39067fefad40ac2859cae474
Saad <66124515+sadsultan@users.noreply.github.com>
2023-09-23 13:49:50 +0500

    Meme component complete

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,39 +1,64 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Meme.css';
 
 export default function Meme(){
-    const [meme, changeMeme] = React.useState('www.google.com/meme.jpg');
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        memeImage: "http://i.imgflip.com/1bij.jpg"
+    });
+
+    const [allMemes, setAllMemes] = React.useState([]);
+
+    React.useEffect(() => {
+        async function getMeme() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMeme()
+    }, [])
 
     function newMeme(){
-        changeMeme(() => `www.google.com/meme${Math.floor(Math.random() * 8)}.jpg`)
+        const num = Math.floor(Math.random() * allMemes.length);
+        const newUrl = allMemes[num].url;
+        setMeme(prev => ({
+                ...prev,
+                memeImage: newUrl
+            })
+        )
+    }
+
+    function handleChange(event){
+        const {name, value} = event.target;
+        setMeme( prev =>({
+                ...prev,
+                [name]: value
+            })
+        )
     }
 
     return(
         <main>
             <div className='form'>
-                <input type="text" placeholder='Top Text' className='form-input' />
-                <input type="text" placeholder='Bottom Text' className='form-input' />
-                <button type="button" className='form-button' onClick={newMeme}>Get a new meme image 🖼</button>
+                <input type="text" placeholder='Top Text' className='form-input' 
+                name='topText'
+                value={meme.topText}
+                onChange={handleChange}/>
+
+                <input type="text" placeholder='Bottom Text' className='form-input' 
+                name='bottomText'
+                value={meme.bottomText}
+                onChange={handleChange}/>
+
+                <button type="button" className='form-button' onClick={newMeme}>Get a new meme image</button>
             </div>
-            <MemeImage meme={meme} />
+
+            <div className='meme'>
+                <img className='meme-img' src={meme.memeImage}/>
+                <h2 className='top-text'>{meme.topText}</h2>
+                <h2 className='bottom-text'>{meme.bottomText}</h2>
+            </div>
         </main>
     )
-}
-
-function MemeImage(props){
-    console.log(props.meme);
-    if (!props.meme) throw new Error('Error in finding meme');
-    console.log(props.meme);
-    return(
-        <a href={props.meme} target='_blank' rel='noopener noreferrer'>
-            <img src={props.meme} alt='meme' className='meme-image' />
-        </a>
-    )
-
-}
-
-// Could be avoided with typescript :( 
-MemeImage.propTypes = {
-    meme: PropTypes.string.isRequired
 }
